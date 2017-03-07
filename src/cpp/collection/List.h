d7e7638f728719f1df5c20717df8ed64a2c62efd
Dmitry Mikhaylovich <dmitry.mikhailovich@gmail.com>
2017-03-07 18:15:30 +0300

    описал List

---
+++ src/cpp/collection/List.h
@@ -0,0 +1,73 @@
+//
+// Created by dmitry on 3/7/17.
+//
+
+#ifndef MIPT_CPP_COLLECTION_H
+#define MIPT_CPP_COLLECTION_H
+
+
+#include <cstdlib>
+
+typedef int element;
+
+class List {
+public:
+    bool contains(element e);
+
+    virtual size_t size() = 0;
+
+    virtual void add(element e) = 0;
+
+    virtual element get(size_t index) = 0;
+};
+
+class ArrayList : public List {
+private:
+    element *values;
+    size_t length;
+    size_t capacity;
+
+    void set_capacity(size_t capacity)
+
+public:
+    ArrayList(size_t length);
+
+    ~ArrayList();
+
+    void add(element e);
+
+    size_t size();
+
+    element get(size_t index);
+};
+
+struct Node {
+    element value;
+    Node *next;
+    Node *prev;
+
+    Node(Node *prev, Node *next, element e);
+};
+
+class LinkedList : public List {
+private:
+    Node *first;
+    Node *last;
+    size_t length;
+public:
+    LinkedList();
+
+    ~LinkedList();
+
+    void add(element e);
+
+    size_t size();
+
+    element get(size_t index);
+
+    element pop();
+};
+
+
+
+#endif //MIPT_CPP_COLLECTION_H
--- src/cpp/collection/collection.cpp
+++
@@ -1,4 +0,0 @@
-//
-// Created by dmitry on 3/7/17.
-//
-
