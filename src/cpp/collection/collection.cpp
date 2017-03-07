13d51b44c7a54a9be1243a7b0ee3480f9f9b105f
Dmitry Mikhaylovich <dmitry.mikhailovich@gmail.com>
2017-03-07 17:30:49 +0300

    добавил collection

---
+++ src/cpp/collection/collection.cpp
@@ -0,0 +1,4 @@
+//
+// Created by dmitry on 3/7/17.
+//
+
---
+++ src/cpp/collection/collection.h
@@ -0,0 +1,14 @@
+//
+// Created by dmitry on 3/7/17.
+//
+
+#ifndef MIPT_CPP_COLLECTION_H
+#define MIPT_CPP_COLLECTION_H
+
+
+class collection {
+
+};
+
+
+#endif //MIPT_CPP_COLLECTION_H
